a828633800a6ca2f08c63488b39939f45e98eacf
Kenny Urena <urenakenny@gmail.com>
2019-08-15 15:59:17 -0400

    initial commit (with framework setting fix)

---
+++ src/QuickStart.Framework/LocalMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickStart.Framework
+{
+    public class LocalMethods
+    {
+        public async Task<object> GetAppDomainDirectory(dynamic input)
+        {
+            return AppDomain.CurrentDomain.BaseDirectory;
+        }
+
+        public async Task<object> GetCurrentTime(dynamic input)
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        public async Task<object> UseDynamicInput(dynamic input)
+        {
+            return $".NET Framework welcomes {input}";
+        }
+    }
+}
